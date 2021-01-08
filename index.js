9e97c468e0bd64d8de95806993d317e87793fe51
Patrick Volker <pat@patrickvolker.com>
2021-01-08 09:43:02 -0500

    added a .gitignore and .env file

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const mysql =  require('mysql');
 
 const app = express();
+const dbPassword = process.env.dbPassword;
 
+//create connection
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'pat',
-    password: 'thegeniesnamewasnotthis',
+    password: dbPassword,
     database: 'runcounter'
 });
 
+//connect
 db.connect();
 
 app.get('/users', (req, res) => {
